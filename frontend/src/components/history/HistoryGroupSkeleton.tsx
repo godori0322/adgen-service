fa2126e5f882a5459f08c434004d2518357db58d
nnnnnnniam1 <nnnnnnniam1@gmail.com>
2025-12-03 09:48:00 +0900

    fix(front-edit): 히스토리 디자인 수정

--- frontend/src/components/history/HistoryGroupSkeleton.tsx
+++ frontend/src/components/history/HistoryGroupSkeleton.tsx
@@ -7,32 +7,31 @@ export default function HistoryGroupSkeleton() {
       {/* 리스트 아이템 3개 */}
       <div className="flex flex-col gap-6">
         {[1, 2, 3].map((_, i) => (
-          <div
-            key={i}
-            className="relative flex gap-5 items-start rounded-2xl bg-white
-                       shadow-sm border border-gray-200 p-5"
-          >
-            {/* 썸네일 스켈레톤 */}
-            <div className="w-48 h-48 bg-gray-200 rounded-xl shrink-0" />
+          <div key={i} className="flex sm:flex-row flex-col items-start gap-2">
+            {/* 썸네일 */}
+            <div className="relative w-48 h-48 rounded-xl bg-gray-200 shrink-0" />
 
-            {/* 텍스트 영역 */}
-            <div className="flex flex-col flex-1 min-w-0 pr-12 justify-between">
-              {/* 날짜 */}
-              <div className="w-24 h-4 bg-gray-200 rounded mb-4" />
+            {/* 텍스트 카드 */}
+            <div
+              className="bg-white border border-gray-200 rounded-xl shadow-[0_2px_6px_rgba(0,0,0,0.05)] 
+                          p-4 flex flex-col gap-2 w-full min-h-48"
+            >
+              {/* 날짜 스켈레톤 */}
+              <div className="w-20 h-3 bg-gray-200 rounded" />
 
-              {/* 아이디어 텍스트 3줄 */}
-              <div className="space-y-2">
-                <div className="w-full h-4 bg-gray-200 rounded" />
-                <div className="w-4/5 h-4 bg-gray-200 rounded" />
-                <div className="w-2/3 h-4 bg-gray-200 rounded" />
+              {/* 아이디어 스켈레톤 */}
+              <div className="space-y-2 mt-2">
+                <div className="w-full h-3 bg-gray-200 rounded" />
+                <div className="w-5/6 h-3 bg-gray-200 rounded" />
+                <div className="w-2/3 h-3 bg-gray-200 rounded" />
               </div>
-            </div>
 
-            {/* 아이콘 영역 */}
-            <div className="absolute bottom-4 right-4 flex gap-2">
-              <div className="w-6 h-6 bg-gray-200 rounded-full" />
-              <div className="w-6 h-6 bg-gray-200 rounded-full" />
-              <div className="w-6 h-6 bg-gray-200 rounded-full" />
+              {/* 아이콘 스켈레톤 - 카드 아래 정렬 */}
+              <div className="mt-auto flex gap-2 opacity-60">
+                <div className="w-5 h-5 bg-gray-200 rounded-full" />
+                <div className="w-5 h-5 bg-gray-200 rounded-full" />
+                <div className="w-5 h-5 bg-gray-200 rounded-full" />
+              </div>
             </div>
           </div>
         ))}
